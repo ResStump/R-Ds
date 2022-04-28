e070421f22a0bad4c7d2df080daf6aaa8f5fa054
Andres Stump <104258039+ResStump@users.noreply.github.com>
2022-04-28 17:02:42 +0200

    Additional plot and various other changes
    - New plot of fitting result, where the data is split up in the different signals
    - Change of the minimizing procedure
    - Other small changes

--- Data_analysis/zfit_on_reco_test.py
+++ Data_analysis/zfit_on_reco_test.py
@@ -8,14 +8,36 @@ import os
 os.environ["ZFIT_DISABLE_TF_WARNINGS"] = "1" # disables first zfit warning
 import zfit
 
-def plot_hist_model(model, data_bin):
-    mplhep.histplot(model.to_hist(), label='model')
-    mplhep.histplot(data_bin.to_hist(), label='data')
+
+######################
+# Plotting functions #
+######################
+
+
+def plot_hist_model_and_data(model, data_zfit):
+    """Plots a histogram of the model with the current parameters and of 
+    data_zfit"""
+    mplhep.histplot(model.to_hist(), label='model', yerr=False)
+    mplhep.histplot(data_zfit.to_hist(), label='data', yerr=False)
+    plt.ylabel('Number of events')
     plt.legend()
     return
 
-# plot profile
+def plot_hist_data_sig(obs):
+    """Plots a stacked histogram of the different signals for the observable
+    obs. The number of events per signal are the current parameters"""
+    bins = hists[obs]['-1'].to_numpy()[1]
+    counts = [params[sig].value()*hists[obs][sig].density()
+              for sig in keys_sig]
+    mplhep.histplot(counts, bins, stack=True, label=labels.values(),
+                    histtype='fill')
+    plt.xlabel(obs)
+    plt.ylabel('Number of events')
+    plt.legend()
+    return
+
 def plot_profile(param, range, NLL, name=None, num=50):
+    """Plots the profile of the parameter param"""
     param_result = param.value()
     NLL0 = NLL.value()
     x = np.linspace(float(range[0]), float(range[1]), num=num)
@@ -35,9 +57,9 @@ def plot_profile(param, range, NLL, name=None, num=50):
     plt.ylabel('$2*\Delta\,$NLL')
     return
 
-# plot contour
 def plot_contour(param1, param2, range1, range2, NLL, xlabel=None, ylabel=None,
-                 num=25):
+                 num=50):
+    """Plots the contour of the parameters param1, param2"""
     param1_result = param1.value()
     param2_result = param2.value()
     NLL0 = NLL.value()
@@ -70,32 +92,54 @@ def plot_contour(param1, param2, range1, range2, NLL, xlabel=None, ylabel=None,
     return
 
 
-# %%#########################
-# Data import and filtering #
-#############################
+# %%#####################################
+# Data import, processing and filtering #
+#########################################
 
-# list of branches in the tree that are used
-keys_tree = ['q2', 'ds_m_mass', 'm2_miss', 'sig']
+# import of CMS Data (here, CMS data is simulated by the Reco data)
+###################################################################
 
-# list of observables that are used from the tree for the optimization
-keys_obs = ['q2', 'm2_miss']
+# list of branches in the CMS tree that are used
+keys_tree_CMS = ['q2', 'e_star_mu3'] # observables
+keys_tree_CMS += ['ds_m_mass'] # filtering
+keys_tree_CMS += ['ds_m_pt', 'mu_pt', 'kp_pt', 'km_pt', 'pi_pt'] # pt_miss
 
-# list of signals in the tree
-keys_sig = ['-1', '0', '1', '2', '3']
+# open CMS root file using lazy and save required branches in the CMS tree
+lazy_tree_CMS = uproot.lazy("ROOT_data/reco_plus_gen_ntuple.root:tree")
+tree_CMS = {key: lazy_tree_CMS[key].to_numpy() for key in keys_tree_CMS}
 
-# open root files using lazy and save required branches in tree
-lazy_tree = uproot.lazy("ROOT_data/reco_plus_gen_ntuple.root:tree")
-tree = {key: lazy_tree[key].to_numpy() for key in keys_tree}
 
-# total number of events
-N_tot = tree['q2'].size
+# import of Reco Data (for bilding the zfit model)
+##################################################
+
+# list of branches in the reco tree that are used
+keys_tree_Reco = keys_tree_CMS + ['sig']
+
+# open reco root file using lazy and save required branches in the reco tree
+lazy_tree_Reco = uproot.lazy("ROOT_data/reco_plus_gen_ntuple.root:tree")
+tree_Reco = {key: lazy_tree_Reco[key].to_numpy() for key in keys_tree_Reco}
+
+
+# Processing
+############
+
+# constants
+m_Bs = 5.36688 # [GeV]
+
+# add pt_miss to reco tree and to CMS data tree
+tree_Reco['pt_miss'] = tree_Reco['ds_m_pt']*m_Bs/tree_Reco['ds_m_mass'] \
+                       - tree_Reco['km_pt'] - tree_Reco['kp_pt'] \
+                       - tree_Reco['pi_pt']  - tree_Reco['mu_pt']
+tree_CMS['pt_miss'] = tree_CMS['ds_m_pt']*m_Bs/tree_CMS['ds_m_mass'] \
+                      - tree_CMS['km_pt'] - tree_CMS['kp_pt'] \
+                      - tree_CMS['pi_pt']  - tree_CMS['mu_pt']
 
 
 # Filtering
 ###########
 
 # index for which bool_arr != True are set to nan in tree
-def tree_filtering(bool_arr):
+def tree_filtering(tree, bool_arr):
     bool_arr = np.logical_not(bool_arr) # inverting the condition
     for k in tree.keys():
         if k == 'sig': # don't apply filter to 'sig'
@@ -103,14 +147,18 @@ def tree_filtering(bool_arr):
         tree[k][bool_arr] = np.nan
     return
 
-filters = dict()
+filters_Reco = dict()
+filters_CMS = dict()
 # cut for B_s mass at 5.36688 GeV (PDG, Fit)
 m_Bs = 5.36688 # [GeV]
-filters['ds_m_mass'] = tree['ds_m_mass'] <= m_Bs
+filters_Reco['ds_m_mass'] = tree_Reco['ds_m_mass'] <= m_Bs
+filters_CMS['ds_m_mass'] = tree_CMS['ds_m_mass'] <= m_Bs
 
 def filterAll():
-    for bool_arr in filters.values():
-        tree_filtering(bool_arr)
+    for bool_arr in filters_Reco.values():
+        tree_filtering(tree_Reco, bool_arr)
+    for bool_arr in filters_CMS.values():
+        tree_filtering(tree_CMS, bool_arr)
     return
 
 filterAll()
@@ -120,35 +168,63 @@ filterAll()
 # Parameters #
 ##############
 
+# total number of events in data
+N_tot = tree_CMS['q2'].size - np.isnan(tree_CMS['q2']).sum()
+
+# list of observables that are used from the trees for the optimization
+keys_obs = ['q2', 'e_star_mu3']
+
+# list of signals in the tree
+keys_sig = ['-1', '0', '1', '2', '3']
+
+# labels of the different channels
+labels = {'-1': 'background',
+          '0': r"$D_s \, \mu \nu$ channel",
+          '1': r"$D_s^* \mu \nu$ channel",
+          '2': r"$D_s \, \tau \nu$ channel",
+          '3': r"$D^*_s \tau \nu$ channel"}
+
 # True parameters
-N_true = {sig: (tree['sig']==int(sig)).sum() for sig in keys_sig}
-R_true = N_true['2']/N_true['0']
-R_star_true = N_true['3']/N_true['1']
+N_sig_true = {sig: (tree_Reco['sig']==int(sig)).sum() for sig in keys_sig}
+R_true = N_sig_true['2']/N_sig_true['0']
+R_star_true = N_sig_true['3']/N_sig_true['1']
+N_true = N_sig_true['0']+N_sig_true['2']
+N_star_true = N_sig_true['1']+N_sig_true['3']
 
 
-# Parameters that are optimized
-R = zfit.Parameter('R', R_true + 0.01, 0, 100)
-R_star = zfit.Parameter('R_star', R_star_true + 0.02, 0, 100)
-N = zfit.Parameter('N', (N_true['0']+N_true['2'])+50000, 0, 1e6,
-                   step_size=1) # N_0 + N_2
-N_star = zfit.Parameter('N_star', (N_true['1']+N_true['3'])-80000, 0, 1e6,
-                        step_size=1) # N_1 + N_3
+# parameters can only be allocated once -> only run this part of the code, if
+# the parameters don't already exist
+try:
+    R
+except NameError:
+    # Parameters that are optimized
+    R = zfit.Parameter('R', R_true + 0.02, -1, 100)
+    R_star = zfit.Parameter('R_star', R_star_true - 0.01, -1, 100)
+    N = zfit.Parameter('N', N_true + 5000, 0, 1e6, step_size=1) # N_0 + N_2
+    N_star = zfit.Parameter('N_star', N_star_true - 8000, 0, 1e6,
+                            step_size=1) # N_1 + N_3
 
-# Composed parameters
-N_m1 = zfit.ComposedParameter('N_m1', lambda N, N_star: N_tot - N - N_star,
-                              params = [N, N_star])
-N_0 = zfit.ComposedParameter('N_0', lambda R, N: N/(R+1),
-                             params=[R, N]) # signal 0
-N_1 = zfit.ComposedParameter('N_1', lambda R_star, N_star: N_star/(R_star+1),
-                             params=[R_star, N_star]) # signal 1
-N_2 = zfit.ComposedParameter('N_2', lambda R, N: R*N/(R+1),
-                             params=[R, N]) # signal 2
-N_3 = zfit.ComposedParameter('N_3',
-                             lambda R_star, N_star: R_star*N_star/(R_star+1),
-                             params=[R_star, N_star]) # signal 3
+    # Composed parameters
+    N_m1 = zfit.ComposedParameter(
+        'N_m1', lambda N, N_star: N_tot - N - N_star, params = [N, N_star]
+    ) # background
+    N_0 = zfit.ComposedParameter(
+        'N_0', lambda R, N: N/(R+1), params=[R, N]
+    ) # signal 0
+    N_1 = zfit.ComposedParameter(
+        'N_1', lambda R_star, N_star: N_star/(R_star+1),
+        params=[R_star, N_star]
+    ) # signal 1
+    N_2 = zfit.ComposedParameter(
+        'N_2', lambda R, N: R*N/(R+1), params=[R, N]
+    ) # signal 2
+    N_3 = zfit.ComposedParameter(
+        'N_3', lambda R_star, N_star: R_star*N_star/(R_star+1),
+        params=[R_star, N_star]
+    ) # signal 3
 
-# parameters used to build the model
-params = {'-1': N_m1, '0': N_0, '1': N_1, '2': N_2, '3': N_3}
+    # parameters used to build the model
+    params = {'-1': N_m1, '0': N_0, '1': N_1, '2': N_2, '3': N_3}
 
 # gaussian constraints
 R_exp = R_true # expected value
@@ -157,38 +233,40 @@ DeltaR = R_true*0.1 # 10% uncertainty for R_exp
 R_star_exp = R_star_true # expected value
 DeltaR_star = R_star_true*0.1 # 10% uncertainty for R_star_exp
 
-constraints = [zfit.constraint.GaussianConstraint(R, R_exp, DeltaR),
+constraints = []
+""" # [zfit.constraint.GaussianConstraint(R, R_exp, DeltaR),
                zfit.constraint.GaussianConstraint(R_star, R_star_exp,
-                                                  DeltaR_star)]
+                                                  DeltaR_star)]"""
 
 
-# %%#################################################
-# creation of zfit model from Data and minimization #
-#####################################################
+# %%################################
+# creation of zfit model from Data #
+####################################
 
 # range of the observables
-ranges = {obs: (np.nanmin(tree[obs]), np.nanmax(tree[obs]))
+ranges = {obs: (np.nanmin(tree_Reco[obs]), np.nanmax(tree_Reco[obs]))
           for obs in keys_obs}
 
-# todo from here!!!!!!
 
-# number of Bins
-bins = {'obs1': 50, 'obs2': 50}
+# number of bins
+# only works if number of bins are all equal?
+bins = {obs: 25 for obs in keys_obs}
 
-# create hists from 'gen' data
+# create hists from reco data
 hists = dict()
 for obs in keys_obs:
     hists[obs] = dict()
     for sig in keys_sig:
         hists[obs][sig] = hist.Hist(hist.axis.Regular(bins[obs], *ranges[obs],
                                                       name=obs))
-        hists[obs][sig].fill(tree[obs][tree['sig']==int(sig)])
+        hists[obs][sig].fill(tree_Reco[obs][tree_Reco['sig']==int(sig)])
 
 
 # plot of hists
 def obs_plot_sig(obs):
     for sig in keys_sig:
-        hists[obs][sig].plot(density=True)
+        hists[obs][sig].plot(density=True, yerr=False, label=f'signal {sig}')
+    plt.legend()
     plt.show()
     return
 
@@ -203,7 +281,7 @@ for obs in keys_obs:
     histPDFs[obs] = zfit.pdf.BinnedSumPDF(
         [zfit.pdf.HistogramPDF(hists[obs][sig]) for sig in keys_sig],
         fracs=list(params.values())
-        )
+    )
 
 # creating observables in zfit (binned)
 binning = {obs: zfit.binned.RegularBinning(bins[obs], *ranges[obs], name=obs)
@@ -212,8 +290,8 @@ all_obs = {obs: zfit.Space(obs, ranges[obs], binning=binning[obs])
            for obs in keys_obs}
 
 # load data into zfit (binned)
-data_zfit = {obs_name: zfit.Data.from_numpy(obs, tree_Data[obs_name])
-             .to_binned(obs)
+data_zfit = {obs_name: zfit.Data.from_numpy(obs, tree_CMS[obs_name])
+                       .to_binned(obs)
              for obs_name, obs in all_obs.items()}
 
 
@@ -221,48 +299,87 @@ data_zfit = {obs_name: zfit.Data.from_numpy(obs, tree_Data[obs_name])
 NLL = zfit.loss.BinnedNLL(model=histPDFs.values(), data=data_zfit.values(),
                           constraints=constraints)
 
-# set parameter values back
-R.set_value(R_true+1)
-R_star.set_value(R_star_true+0.7)
-N.set_value((N_0_true+N_2_true)+20000)
-N_star.set_value((N_1_true+N_2_true)-10000)
+# set initial parameter values
+"""R.set_value(R_true+0.05)
+R_star.set_value(R_star_true-0.03)
+N.set_value(N_true+50000)
+N_star.set_value(N_star_true-80000)"""
+
+
+# %%############
+# miminization #
+################
 
 # plots before minimization
-for obs in keys_obs:
-    plot_hist_model(histPDFs[obs], data_zfit[obs])
+"""for obs in keys_obs:
+    plot_hist_model_and_data(histPDFs[obs], data_zfit[obs])
     plt.title('Befor minimizing')
-    plt.show()
+    plt.show()"""
 
-# miminization
-minimizer = zfit.minimize.Minuit()
+# selection of the minimizer
+minimizer = zfit.minimize.Minuit()#gradient=True)
+
+# run minimizer until it converged and until the parameters don't change
+# much anymore
 result = minimizer.minimize(NLL)
-result.hesse()
+n = 1
+temp_results = np.array(result.values)
+params_stable = False
+while not result.converged and not params_stable:
+    result = minimizer.minimize(NLL)
+    n += 1
+    params_stable = np.allclose(np.array(result.values), temp_results)
+    temp_results = np.array(result.values)
+
+print(f'Number of iterations until the minimizer converged: {n}')
 
 # plots after minimization
-for obs in keys_obs:
-    plot_hist_model(histPDFs[obs], data_zfit[obs])
+"""for obs in keys_obs:
+    plot_hist_model_and_data(histPDFs[obs], data_zfit[obs])
     plt.title('After minimizing')
+    plt.show()"""
+
+
+# %%#######
+# Results #
+###########
+
+# plot of the result split into signals
+for obs in keys_obs:
+    plot_hist_data_sig(obs)
     plt.show()
 
+# calculate uncertainty of result
+result.hesse()
+
+# results
 print(result)
+R_res = result.values[R]
+DeltaR_res = result.hesse()[R]['error']
+R_star_res = result.values[R_star]
+DeltaR_star_res = result.hesse()[R_star]['error']
+
+# ranges for plotting
+R_range = (R_res - 1.5*DeltaR_res, R_res + 1.5*DeltaR_res)
+R_star_range = (R_star_res - 1.5*DeltaR_star_res,
+                R_star_res + 1.5*DeltaR_star_res)
+
 
 # plot of profile of R
-plot_profile(R, (R.value()-0.05, R.value()+0.05), NLL)
+plot_profile(R, R_range, NLL)
 plt.minorticks_on()
 plt.grid(True)
 plt.show()
 
 # plot of profile of R_star
-plot_profile(R_star, (R_star.value()-0.05, R_star.value()+0.05), NLL,
-             name='$R^*$')
+plot_profile(R_star, R_star_range, NLL, name='$R^*$')
 plt.minorticks_on()
 plt.grid(True)
 plt.show()
 
 # plot contour of R, R_star
-plot_contour(R, R_star, range1=(R.value()-0.05, R.value()+0.05),
-             range2=(R_star.value()-0.05, R_star.value()+0.05),
-             NLL=NLL, ylabel='$R^*$')
+plot_contour(R, R_star, range1=R_range, range2=R_star_range, NLL=NLL,
+             ylabel='$R^*$')
 plt.minorticks_on()
 plt.grid(True)
 plt.show()
@@ -272,6 +389,10 @@ plt.show()
 # ToDos #
 #########
 # - Create selection to handle (artificial) K pi ambiguity
+# - Problem: # bin for the two obs somehow have to be equal,
+#   but should not be!!!!!
+# - Also plot 2 sigma contour? i.e. 2*DeltaNLL = 4 = 2**2
+# - Look at error from hesse(), does it change when multiple times applied
 
 
 
